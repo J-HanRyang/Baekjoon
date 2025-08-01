bea576e9b4c641d7313562b7765181c7ea58a7a8
J-HanRyang <162584369+J-HanRyang@users.noreply.github.com>
2025-08-01 12:47:43 +0900

    [Silver IV] Title: 수 찾기, Time: 796 ms, Memory: 55936 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,36 @@
+N = int(input())
+array = []
+temp = input().split()
+
+for i in range (N) :
+    array.append(int(temp[i]))
+
+array.sort()
+
+M = int(input())
+data = []
+temp2 = input().split()
+
+for i in range (M) :
+    data.append(int(temp2[i]))
+
+for i in range (M) :
+    search = data[i]
+    low_index = 0
+    high_index = N-1
+    is_find = False
+    
+    while low_index <= high_index :
+        mid_index = (low_index + high_index) // 2
+        
+        if search > array[mid_index] :
+            low_index = mid_index + 1
+        elif search < array[mid_index] :
+            high_index = mid_index - 1
+        else :
+            print("1")
+            is_find = True
+            break
+
+    if not is_find :
+        print("0")
